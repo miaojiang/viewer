b14fa1e09e7132db0ee3954afc1f0b55740f1dfb
MJ <mj@proximity.dev>
2024-01-26 12:27:01 -0800

    removed meteor due to package conflict

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { setupNearWallet } from "@near-wallet-selector/near-wallet";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
 import { setupSender } from "@near-wallet-selector/sender";
 import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+//import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 import { setupNightly } from "@near-wallet-selector/nightly";
 import { setupModal } from "@near-wallet-selector/modal-ui";
 import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
@@ -75,7 +75,7 @@ function App(props) {
           }),
             setupSender(),
             setupHereWallet(),
-            setupMeteorWallet(),
+            //setupMeteorWallet(),
             setupNightly(),
           ],
         }),
